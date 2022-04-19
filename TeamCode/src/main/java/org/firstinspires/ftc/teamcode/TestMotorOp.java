8d5c748e9d8093a91027fc0bf01baf35b67e3b0a
sammypbaird <sammypbaird@gmail.com>
2022-04-18 20:28:04 -0600

    Adding first sample motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotorOp.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Test Motor", group="Linear Opmode")
+public class TestMotorOp extends LinearOpMode  {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftBackDrive = null;
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        //set the direction
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        double power = 0;
+        while (opModeIsActive()) {
+            power = 0.5;
+
+            
+            leftBackDrive.setPower(power);
+            leftFrontDrive.setPower(power);
+            rightBackDrive.setPower(power);
+            rightFrontDrive.setPower(power);
+
+            telemetry.addData("Target Power", power);
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
