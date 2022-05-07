5575cc3e9b5ac7f13b7ab08076ad5a4736961955
sammypbaird <sammypbaird@gmail.com>
2022-05-07 11:42:35 -0600

    Adding a basic Driver class that can be used for simple driving functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriverOp.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.objects.Driver;
+
+@TeleOp(name="Test Driver Op", group="Linear Opmode")
+public class TestDriverOp extends LinearOpMode {
+
+    private Driver driver = null;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        driver = new Driver(hardwareMap);
+        driver.setPower(0.5);
+
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            driver.driveForwardForDurationMs(2000);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/objects/Driver.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.objects;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Driver {
+
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    private double power = 0;
+
+    public Driver(HardwareMap hardwareMap)
+    {
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        // Most robots need the motors on one side to be reversed to drive forward.
+        // When you first test your robot, push the left joystick forward
+        // and flip the direction ( FORWARD <-> REVERSE ) of any wheel that runs backwards
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        motorsStop();
+    }
+
+    public void setPower(double p)
+    {
+        this.power = p;
+    }
+
+    public void driveForwardForDurationMs(long duration)
+    {
+        long start = System.currentTimeMillis();
+        while (System.currentTimeMillis() < start + duration) {
+            motorsForward();
+        }
+
+        motorsStop();
+    }
+
+    private void motorsForward()
+    {
+        setPower(power, power, power, power);
+    }
+
+    private void motorsStop()
+    {
+        setPower(0,0,0,0);
+    }
+
+    private void setPower(double leftFront, double rightFront, double leftBack, double rightBack)
+    {
+        leftFrontDrive.setPower(leftFront);
+        rightFrontDrive.setPower(rightFront);
+        leftBackDrive.setPower(leftBack);
+        rightBackDrive.setPower(rightBack);
+    }
+}
