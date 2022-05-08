c893f2bd1623390a2422bbeed928e6eb7279129f
sammypbaird <sammypbaird@gmail.com>
2022-05-07 22:04:22 -0600

    Adding an "OmniDriver" exercise, with comments and instructions to implement it.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/objects/OmniDriver.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode.objects;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * OmniDriver can receive basic driving commands, and it sets the motors power appropriately.
+ * This driver has 3 degrees of movement:
+ *  - axial: movement forwards and backwards
+ *  - lateral: movement left and right
+ *  - yaw: rotation left and right
+ */
+public class OmniDriver {
+
+    //store the DcMotors as class variables
+
+    //store the speed
+
+    public OmniDriver(HardwareMap hardwareMap)
+    {
+        // 2. Initialize the DcMotors by looking them up in the hardwareMap
+
+
+        // 3. Set the motor directions (reverse the left motors)
+    }
+
+    /**
+     * Sets the speed of the driver
+     * @param speed Must be between 0 and 1
+     */
+    public void setSpeed(double speed)
+    {
+        //assign the class variable
+    }
+
+    /**
+     * Stops the driver (which sets the speed to zero)
+     */
+    public void stop()
+    {
+        //set the speed to zero
+
+        //stop the motors by calling "setDirection"
+    }
+
+    /**
+     * Set the direction of the driver, by providing 3 degrees of movement. All numbers
+     * will be combined to generate an aggregate movement.
+     * @param axial Backward/Forward movement, number must be between -1 and 1
+     * @param lateral Left/Right movement, number must be between -1 and 1
+     * @param yaw Rotate Left/Rotate Right, number must be between -1 and 1
+     */
+    public void setDirection(double axial, double lateral, double yaw)
+    {
+        //calculate the power to each motor
+
+        //normalize the values
+
+        //multiple the values by the speed
+
+        //set the power to each motor
+    }
+
+    public void forward(long milliseconds)
+    {
+        //use "setDirection" and "waitMs" to accomplish this
+    }
+
+    public void backward(long milliseconds)
+    {
+        //use "setDirection" and "waitMs" to accomplish this
+    }
+
+    public void strafeLeft(long milliseconds)
+    {
+        //use "setDirection" and "waitMs" to accomplish this
+    }
+
+    public void strafeRight(long milliseconds)
+    {
+        //use "setDirection" and "waitMs" to accomplish this
+    }
+
+    public void rotateLeft(long milliseconds)
+    {
+        //use "setDirection" and "waitMs" to accomplish this
+    }
+
+    public void rotateRight(long milliseconds)
+    {
+        //use "setDirection" and "waitMs" to accomplish this
+    }
+
+    private void waitMs(long milliseconds)
+    {
+        //wait until milliseconds
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmode/TestDriverOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmode/TestDriverOp.java
@@ -7,18 +7,19 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.objects.Driver;
+import org.firstinspires.ftc.teamcode.objects.OmniDriver;
 
 @Autonomous(name="Test Driver Op", group="")
 public class TestDriverOp extends LinearOpMode {
 
-    private Driver driver = null;
+    private OmniDriver driver = null;
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() {
 
-        driver = new Driver(hardwareMap);
-        driver.setPower(0.5);
+        driver = new OmniDriver(hardwareMap);
+        driver.setSpeed(0.5);
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
@@ -28,15 +29,17 @@ public class TestDriverOp extends LinearOpMode {
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
-        driver.driveForward(1000);
+        driver.forward(1000);
         driver.strafeRight(1000);
-        driver.driveBackwards(1000);
+        driver.backward(1000);
         driver.strafeLeft(1000);
 
         for (int i=0;i<10;i++) {
             driver.rotateRight(500);
-            driver.driveForward(500);
+            driver.forward(500);
         }
+
+        driver.stop();
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
